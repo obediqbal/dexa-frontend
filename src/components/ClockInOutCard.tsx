3e7ce0a70c99c42fa95c4b9f23c2c6e3dec701e2
Rezapu Obed <obed.ubaidillah@gmail.com>
2026-01-04 22:00:23 +0700

    feat: add warning when not clocked in or clocked out

--- src/components/ClockInOutCard.tsx
+++ src/components/ClockInOutCard.tsx
@@ -94,6 +94,19 @@ export function ClockInOutCard({ todayAttendance, onClockAction }: ClockInOutCar
 
     return (
         <div className="clock-card">
+            {(!todayAttendance?.clockIn || !todayAttendance?.clockOut) && (
+                <div className="attendance-warning">
+                    <svg viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                    </svg>
+                    <span>
+                        {!todayAttendance?.clockIn
+                            ? "You haven't clocked in today"
+                            : "You haven't clocked out today"}
+                    </span>
+                </div>
+            )}
+
             <div className="clock-info">
                 <div className="info-row">
                     <span className="info-label">Hari:</span>
